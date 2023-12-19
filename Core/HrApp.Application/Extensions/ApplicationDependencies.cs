4f9ba20d3f79fd406020d6dbbb4a59684885e8b9
mukumbasar <mehmetutkununoyunlari@gmail.com>
2023-12-19 12:19:54 +0300

    First folder structure has been set to work on.

---
+++ Core/HrApp.Application/Extensions/ApplicationDependencies.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrApp.Application.Extensions
+{
+    public static class ApplicationDependencies
+    {
+        public static void AddApplicationDependencies(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
---
+++ Infrastructure/HrApp.Persistence/Extensions/PersistenceDependencies.cs
@@ -0,0 +1,29 @@
+﻿using HrApp.Application.Interfaces;
+using HrApp.Persistence.Context;
+using HrApp.Persistence.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrApp.Persistence.Extensions
+{
+    public static class PersistenceDependencies
+    {
+        public static void AddPersistenceDependencies(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<HrAppDbContext>(x =>
+            {
+                x.UseSqlServer(connectionString);
+            });
+
+            #region Uow and Repositories
+
+            services.AddScoped<IUow, Uow>();
+            #endregion
+        }
+    }
+}
