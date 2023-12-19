e74aba30669f966dc85ce56075425aac523db12e
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-19 14:15:04 +0300

    Connection String added to project. Persistence dependencies injected.

--- Presentation/HrApp.WebAPI/Program.cs
+++ Presentation/HrApp.WebAPI/Program.cs
@@ -1,8 +1,12 @@
+using HrApp.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddPersistenceDependencies(builder.Configuration.GetConnectionString("DevConnection"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +16,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   app.UseSwagger();
+   app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
