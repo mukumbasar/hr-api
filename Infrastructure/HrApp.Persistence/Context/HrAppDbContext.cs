d3fbf64e3b9ae1e7ccba91ff12bcdd7c197ef1dc
mukumbasar <mehmetutkununoyunlari@gmail.com>
2023-12-19 11:48:07 +0300

    The bases for HrAppDbContext, Uow and GenericRepository have been set up.

---
+++ Infrastructure/HrApp.Persistence/Context/HrAppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HrApp.Persistence.Context
+{
+    public class HrAppDbContext : DbContext
+    {
+
+        public HrAppDbContext(DbContextOptions<HrAppDbContext> option) : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Infrastructure/HrApp.Persistence/UnitOfWork/Uow.cs
@@ -0,0 +1,27 @@
+﻿using HrApp.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrApp.Persistence.UnitOfWork
+{
+    public class Uow
+    {
+        private readonly HrAppDbContext _context;
+
+
+        public Uow(HrAppDbContext context
+           )
+        {
+            _context = context;
+
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
