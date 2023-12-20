2b34ea504a853d05961ca904395f692a86e8536f
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-20 10:47:09 +0300

    AppUserDtos-added

---
+++ Core/HrApp.Application/Dtos/AppUserDetailsDto.cs
@@ -0,0 +1,22 @@
+﻿namespace HrApp.Application;
+
+public class AppUserDetailsDto
+{
+   public string Name { get; set; }
+   public string? SecondName { get; set; }
+   public string Surname { get; set; }
+   public string? SecondSurname { get; set; }
+   public DateTime BirthYear { get; set; }
+   public string BirthPlace { get; set; }
+   public string TurkishIdentificationNumber { get; set; }
+   public DateTime StartDate { get; set; }
+   public DateTime EndDate { get; set; }
+   public bool IsActive { get; set; }
+   public string Department { get; set; }
+   public string CompanyName { get; set; }
+   public string Occupation { get; set; }
+   public string Email { get; set; }
+   public string Address { get; set; }
+   public string MobileNumber { get; set; }
+   public byte[] ImageData { get; set; }
+}
---
+++ Core/HrApp.Application/Dtos/AppUserUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HrApp.Application;
+
+public class AppUserUpdateDto
+{
+   public byte[] ImageData { get; set; }
+   public string Address { get; set; }
+   public string MobileNumber { get; set; }
+}
