831a991881f28b88455ac975c6e3bb52929e2302
cllkbc <burcuclk2020@hotmail.com>
2023-12-20 16:34:24 +0300

    personelcontroller changed

---
+++ Presentation/HrApp.MVC/Models/PersonelHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HrApp.MVC.Models
+{
+    public class PersonelHomeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? SecondName { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public string? SecondSurname { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
