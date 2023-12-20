248b8d32360d75e2d62aba893bb74cffc19792c2
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-20 22:55:31 +0300

    exception Handling middleware added

--- Presentation/HrApp.WebAPI/Program.cs
+++ Presentation/HrApp.WebAPI/Program.cs
@@ -3,6 +3,7 @@ using HrApp.Application.Extensions;
 using Microsoft.EntityFrameworkCore;
 using HrApp.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
+using HrApp.WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,36 +25,7 @@ builder.Services.AddCors(options =>
    });
 });
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-    var RoleManager = scope.ServiceProvider.GetRequiredService<RoleManager<AppRole>>();
-
-    await userManager.CreateAsync(new AppUser()
-    {
-        UserName = "abc",
-        NormalizedUserName = "ABC",
-        Name = "Ad 1",
-        SecondName = "Ad 2",
-        Surname = "Soyad 1",
-        SecondSurname = "Soyad 2",
-        BirthYear = DateTime.Now.AddYears(-30),
-        TurkishIdentificationNumber = "12345123451",
-        StartDate = DateTime.Now.AddYears(-5),
-        EndDate = DateTime.Now.AddYears(-3),
-        IsActive = false,
-        Department = "IT",
-        CompanyName = "BA",
-        Occupation = "Full-stack Developer",
-        Email = "mail@mail.com",
-        NormalizedEmail = "MAIL@MAIL.COM",
-        Address = "Adres 1",
-        MobileNumber = "213451245",
-        Salary = 98000,
-        BirthPlace = "Ankara"
-
-    });
-}
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
