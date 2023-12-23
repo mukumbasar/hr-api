2924c25507f594f97a555c0b6befaae3b8233250
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-23 10:58:05 +0300

    exceptionHandlingMiddleware updated

--- Presentation/HrApp.WebAPI/CustomMiddleware/ExceptionHandlingMiddleware.cs
+++ Presentation/HrApp.WebAPI/CustomMiddleware/ExceptionHandlingMiddleware.cs
@@ -1,22 +1,24 @@
-﻿namespace HrApp.WebAPI;
+﻿using System.Net;
+
+namespace HrApp.WebAPI;
 
 public class ExceptionHandlingMiddleware
 {
-   private readonly RequestDelegate _next;
-   public ExceptionHandlingMiddleware(RequestDelegate next)
-   {
-      _next = next;
-   }
-   public async Task Invoke(HttpContext context)
-   {
-      try
-      {
-         await _next(context);
-      }
-      catch (Exception ex)
-      {
-         //log to console
-         Console.WriteLine(ex.Message);
-      }
-   }
+    private readonly RequestDelegate _next;
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+    }
 }
