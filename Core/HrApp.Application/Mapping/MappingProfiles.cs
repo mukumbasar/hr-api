413c98bb214807d19984f86a8ae110ff0cca4879
mukumbasar <mehmetutkununoyunlari@gmail.com>
2023-12-26 15:53:47 +0300

    The needed mapping profiles have been added.

--- Core/HrApp.Application/Mapping/MappingProfiles.cs
+++ Core/HrApp.Application/Mapping/MappingProfiles.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using HrApp.Application.CQRS.Advance.Commands;
+using HrApp.Application.CQRS.Expense.Commands;
+using HrApp.Application.CQRS.Leave.Commands;
+using HrApp.Application.Dtos;
 using HrApp.Domain.Entities;
 
 namespace HrApp.Application.Mapping
@@ -12,9 +16,21 @@ namespace HrApp.Application.Mapping
    {
       public MappingProfiles()
       {
-         CreateMap<AppUser, AppUserDetailsDto>();
-         CreateMap<AppUser, AppUserHomeDto>();
-         CreateMap<AppUser, AppUserUpdateDto>();
-      }
+            CreateMap<AppUser, AppUserDetailsDto>();
+            CreateMap<AppUser, AppUserHomeDto>();
+            CreateMap<AppUser, AppUserUpdateDto>();
+
+            CreateMap<CreateAdvanceCommand, Advance>();
+            CreateMap<UpdateAdvanceCommand, Advance>();
+            CreateMap<Advance, AdvanceDto>();
+
+            CreateMap<CreateExpenseCommand, Expense>();
+            CreateMap<UpdateExpenseCommand, Expense>();
+            CreateMap<Expense, ExpenseDto>();
+
+            CreateMap<CreateLeaveCommand, Leave>();
+            CreateMap<UpdateLeaveCommand, Leave>();
+            CreateMap<Leave, LeaveDto>();
+        }
    }
 }
