22e7b7b279a6aa27adbd1c59cae8a90c8610407e
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-01-08 11:10:39 +0300

    minor changes.

---
+++ Core/HrApp.Domain/Entities/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrApp.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public int CompanyTypeId { get; set; }
+        public CompanyType CompanyType { get; set; }
+    }
+}
