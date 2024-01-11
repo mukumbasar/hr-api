7e77220c36206d94c4d87b9a3638711acb6b3d7c
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-01-11 09:51:41 +0300

    minor changes on get all app user command

--- Core/HrApp.Domain/Entities/AppUser.cs
+++ Core/HrApp.Domain/Entities/AppUser.cs
@@ -22,7 +22,6 @@ namespace HrApp.Domain.Entities
         public DateTime? EndDate { get; set; }
         public bool IsActive { get { return EndDate == null ? true : false; } }
         public string Department { get; set; }
-        public string CompanyName { get; set; }
         public string Occupation { get; set; }
         public string Address { get; set; }
         public string MobileNumber { get; set; }
