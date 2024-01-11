311078e07f781b3a00baa23d1c1f2efa46dfaceb
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-01-11 09:53:41 +0300

    m change.

--- Core/HrApp.Application/Dtos/AppUserDto.cs
+++ Core/HrApp.Application/Dtos/AppUserDto.cs
@@ -21,6 +21,7 @@ namespace HrApp.Application.Dtos
         public bool IsActive { get { return EndDate == null ? true : false; } }
         public string Department { get; set; }
         public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
         public string Occupation { get; set; }
         public string Address { get; set; }
         public string MobileNumber { get; set; }
